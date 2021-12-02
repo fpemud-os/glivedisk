62711b6b8e0446aa5d8eaed4f809d98cc9657f83
fpemud <fpemud@sina.com>
2021-12-03 01:54:34 +0800

    fix

--- setup.py
+++ setup.py
@@ -44,9 +44,9 @@ setup(
 		],
 	packages=[
 		__package__,
-		'{0}.arch'.format(__package__),
-		'{0}.base'.format(__package__),
-		'{0}.targets'.format(__package__),
+		'{0}.export_target'.format(__package__),
+		# '{0}.arch'.format(__package__),
+		# '{0}.base'.format(__package__),
 		],
     package_dir={
         __package__: os.path.join('python3', __package__),
