0f9f460d38946769a0a9533cd586ddeac6d121b6
fpemud <fpemud@sina.com>
2021-12-13 10:44:52 +0800

    fix

--- python3/glivedisk/scripts-in-chroot/update-system-set.sh
+++ python3/glivedisk/scripts-in-chroot/update-system-set.sh
@@ -4,7 +4,8 @@ source $(dirname $(realpath $0))/functions.sh
 
 export CONFIG_PROTECT="-* /etc/locale.gen"
 
-run_merge "-e --update --deep --with-bdeps=y @system"
+#run_merge "-e --update --deep --with-bdeps=y @system"
+run_merge "--update --deep --with-bdeps=y @system"
 
 # Replace modified /etc/locale.gen with default
 etc-update --automode -5
